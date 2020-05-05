2e5c996c366f4304637f700219ab51a46fdfba4a
clemson tigress <anreeves83@gmail.com>
2020-05-04 20:36:44 -0400

    second commit

---
+++ README copy.md
@@ -0,0 +1,16 @@
+
+
+At least one badge
+## Project title
+## Description
+## Table of Contents
+## Installation
+## Usage
+## License
+## Contributing
+## Tests
+
+## User GitHub profile picture
+## User GitHub email
+
+
--- index.js
+++ index.js
@@ -1,23 +1,9 @@
-//const questions = [
-
-//];
-
-//function writeToFile(fileName, data) {
-//}
-
-//function init() {
-
-//}
-
-//init();
-
-
-const fs = require("fs")
+//const fs = require("fs")
 const inquirer = require("inquirer");
-const util = require("util")
+//const util = require("util")
 const axios = require("axios")
 
-const asyncWriteFile = util.promisify(fs.writeFile)
+//const asyncWriteFile = util.promisify(fs.writeFile)
 
 //GIVEN the developer has a GitHub profile and a repository
 //WHEN prompted for the developer's GitHub username and repo specific information
@@ -25,34 +11,64 @@ const asyncWriteFile = util.promisify(fs.writeFile)
 
 //make a call to the GitHub API to retrieve their email and profile image
 
-inquirer
-  .prompt({
-    type: "input",
-    message: "Enter your GitHub username:",
-    name: "username"
-  })
-  .then(function({ username }) {
-    const queryUrl = `https://api.github.com/users/${username}/repos?per_page=100`;
+function promptUser() {
+    return inquirer.prompt([
+        {
+            type: "input",
+            message: "Enter your GitHub username:",
+            name: "username"
+        },
+        {
+            type: "input",
+            message: "Enter your Project Title:",
+            name: "title"
+        },
+        {
+            type: "input",
+            message: "Enter your Project Description:",
+            name: "description"
+        }
 
-    axios.get(queryUrl).then(function(res) 
-    {
-      const email = res.data.name;
-      });
-
-      
-
-      //fs.writeFile("readme.md", repoNamesStr, function(err) {
-        //if (err) {
-          //throw err;
-        //}
-        //console.log("Success");
-      });
-   
+    ])
+};
 
 
-  function init(){
-      //get user input
-      //use answers to generate read me template
-      //write to file
-      //log success
-  }
+async function gitSearch() {
+    try {
+        const queryUrl = `https://api.github.com/search/users?q=${response.username}`;
+        const response = await axios.get(queryUrl);
+        console.log(repsonse)
+    }
+    catch (err) {
+        console.log(err);
+    };
+};
+
+
+
+
+
+//function init() {
+    //console.log("Hello!")
+    //try{
+    //get user input
+    //const response = await promptUser();
+    //use answers to generate read me template
+    //const queryUrl = `https://api.github.com/search/users?q=${response.username}`;
+    //console.log(response)
+    //const data = await gitSearch(queryUrl);
+    //console.log(data)
+    //const readMe = makeReadMe(answers)
+
+    //write to file
+    //await asyncWriteFile("readme.md", data);
+    //log success
+    //}
+
+    //catch(err) {console.log(err);
+   // }
+//};
+
+//init();
+promptUser();
+gitSearch(response);
